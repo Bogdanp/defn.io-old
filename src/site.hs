7ad6230290f77f0cedfecf5fa7f523d33ef392f5
Bogdan Popa <popa.bogdanp@gmail.com>
2016-09-17 15:50:55 +0300

    hindent site.hs

--- src/site.hs
+++ src/site.hs
@@ -1,100 +1,91 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Hakyll
-
+import Hakyll
 
 main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match "about.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "projects.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archive"             `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $
-          loadAllSnapshots "posts/*" "content"
-            >>= fmap (take 10) . recentFirst
-            >>= renderAtom feedConfiguration feedCtx
-
-    create ["rss.xml"] $ do
-        route idRoute
-        compile $
-          loadAllSnapshots "posts/*" "content"
-            >>= fmap (take 10) . recentFirst
-            >>= renderRss feedConfiguration feedCtx
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- (return . take 10) <$> (recentFirst =<< loadAll "posts/*")
-            let indexCtx =
-                    listField "posts" postCtx posts   `mappend`
-                    constField "title" "Recent posts" `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateCompiler
-
+main =
+    hakyll $
+    do match "images/*" $
+           do route idRoute
+              compile copyFileCompiler
+       match "css/*" $
+           do route idRoute
+              compile compressCssCompiler
+       match "about.md" $
+           do route $ setExtension "html"
+              compile $
+                  pandocCompiler >>=
+                  loadAndApplyTemplate "templates/default.html" defaultContext >>=
+                  relativizeUrls
+       match "projects.md" $
+           do route $ setExtension "html"
+              compile $
+                  pandocCompiler >>=
+                  loadAndApplyTemplate "templates/default.html" defaultContext >>=
+                  relativizeUrls
+       match "posts/*" $
+           do route $ setExtension "html"
+              compile $
+                  pandocCompiler >>=
+                  loadAndApplyTemplate "templates/post.html" postCtx >>=
+                  saveSnapshot "content" >>=
+                  loadAndApplyTemplate "templates/default.html" postCtx >>=
+                  relativizeUrls
+       create ["archive.html"] $
+           do route idRoute
+              compile $
+                  do posts <- recentFirst =<< loadAll "posts/*"
+                     let archiveCtx =
+                             listField "posts" postCtx (return posts) `mappend`
+                             constField "title" "Archive" `mappend`
+                             defaultContext
+                     makeItem "" >>=
+                         loadAndApplyTemplate
+                             "templates/archive.html"
+                             archiveCtx >>=
+                         loadAndApplyTemplate
+                             "templates/default.html"
+                             archiveCtx >>=
+                         relativizeUrls
+       create ["atom.xml"] $
+           do route idRoute
+              compile $
+                  loadAllSnapshots "posts/*" "content" >>=
+                  fmap (take 10) . recentFirst >>=
+                  renderAtom feedConfiguration feedCtx
+       create ["rss.xml"] $
+           do route idRoute
+              compile $
+                  loadAllSnapshots "posts/*" "content" >>=
+                  fmap (take 10) . recentFirst >>=
+                  renderRss feedConfiguration feedCtx
+       match "index.html" $
+           do route idRoute
+              compile $
+                  do posts <-
+                         (return . take 10) <$>
+                         (recentFirst =<< loadAll "posts/*")
+                     let indexCtx =
+                             listField "posts" postCtx posts `mappend`
+                             constField "title" "Recent posts" `mappend`
+                             defaultContext
+                     getResourceBody >>= applyAsTemplate indexCtx >>=
+                         loadAndApplyTemplate "templates/default.html" indexCtx >>=
+                         relativizeUrls
+       match "templates/*" $ compile templateCompiler
 
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
-
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
 
 feedCtx :: Context String
-feedCtx =
-    postCtx `mappend`
-    bodyField "description"
-
+feedCtx = postCtx `mappend` bodyField "description"
 
 feedConfiguration :: FeedConfiguration
-feedConfiguration = FeedConfiguration
-  { feedTitle = "defn.io"
-  , feedDescription = ""
-  , feedAuthorName = "Bogdan Popa"
-  , feedAuthorEmail = "bogdan@defn.io"
-  , feedRoot = "http://defn.io"
-  }
+feedConfiguration =
+    FeedConfiguration
+    { feedTitle = "defn.io"
+    , feedDescription = ""
+    , feedAuthorName = "Bogdan Popa"
+    , feedAuthorEmail = "bogdan@defn.io"
+    , feedRoot = "http://defn.io"
+    }
